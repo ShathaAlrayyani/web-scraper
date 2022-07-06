b13523306b26cf41c28571ca541b46ba5d81e849
ShathaAlrayyani <shatha.alrayyani223@gmail.com>
2022-07-06 06:40:49 +0300

    lab 17

--- scraper.py
+++ scraper.py
@@ -3,33 +3,32 @@ from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
 
-all_jobs = []
-def get_citations_needed_count(url):
-    r = requests.get(url)
-    # print(r.content)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    jobs_cards = soup.find_all('li', class_='has-pointer-d')
-    for citation in job_card:
+def get_citations_needed_count(url: str):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    citation = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
+    count = 0
+    for citation in citation:
+        count += 1
+    # print(f'The number of citations needed in this page is {count}')
+    return f'The number of citations needed for this page is {count}'
 
 
 
+def get_citations_needed_report(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    citations = soup.find_all('a', text='citation needed')
+
+    output = []
+    for citation in citations:
+        text = citation.find_parents('p')[0].text
+        output.append(text)
+    rep = '\n'.join(output)
+    return rep.strip()
 
 
-def get_citations_needed_report():
-    pass
-for job in jobs_cards:
-    title = job.find('a').contents[0].strip()
-    company = job.find('b', class_ = 'jb-company').contents[0].strip()
-    desc = job.find('div', class_ = 'jb-descr').contents[0].strip()
-    all_jobs.append({
-       'title' : title,
-       'company' : company,
-       'desc' : desc
-   })
 
-job_titles = soup.find_all('h2', class_ = 'jb-title')
-print(job_titles[0].text)
-
-# import json
-# with open('jobs.json', 'w') as file:
-#     file.write(json.dumps(all_jobs))
+if __name__ == "__main__":
+    print(get_citations_needed_report(url))
+    print(get_citations_needed_count(url))
